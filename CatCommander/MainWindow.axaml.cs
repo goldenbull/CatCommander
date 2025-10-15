fbebde6f586a12b1a455534069cb275cd1e2fc3b
cjn <cjn@home>
2025-10-15 22:52:29 +0800

    ugly UI update

--- CatCommander/MainWindow.axaml.cs
+++ CatCommander/MainWindow.axaml.cs
@@ -1,14 +1,22 @@
-using System;
 using Avalonia.Controls;
+using CatCommander.ViewModels;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using CatCommander.Services;
 
 namespace CatCommander
 {
     public partial class MainWindow : Window
     {
+        private readonly ILogger<MainWindow> _logger = LoggingService.CreateLogger<MainWindow>();
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+
+            _logger.LogInformation("DPI: {RenderScaling} {DesktopScaling}", this.RenderScaling, this.DesktopScaling);
         }
     }
 }
